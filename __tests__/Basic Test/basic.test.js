33391c94192641c8a2a48808d760d30641974945
Calvin Moore <calvin.moore@allangray.co.za>
2019-04-29 19:18:13 +0200

    touchups

--- __tests__/Basic Test/basic.test.js
+++ __tests__/Basic Test/basic.test.js
@@ -1,10 +1,6 @@
 function sum(a, b, c, d) {
   return a + b + c + d;
 }
-// module.exports = sum;
-
-
-// const sum = require('./sum');
 
 test('adds 1 + 2 to equal 3', () => {
   expect(sum('Nordic ', 'Testing ', 'Days ', '2019')).toBe('Nordic Testing Days 2019');
